d25d4118d98751514806917039cb7962df48b9dd
suseelasurendra <satyasrisuseela@gmail.com>
2024-12-10 13:42:43 +0000

    STOCKS code

--- emp.java
+++ emp.java
@@ -1,3 +1,27 @@
+void sales()
+{
+::::SALES:::
+::::SALES:::
+::::SALES:::
+::::SALES:::
+::::SALES:::
+
+}
+
+void stock()
+{
+...stocks...
+...stocks...
+...stocks...
+...stocks...
+...stocks...
+
+}
+
+
+
+
+
 class emp 
 {
 s.o.print("emp code");
